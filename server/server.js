03be8cbb8e2b4dfe736b6ae40e48443a76165ca6
ansshul10 <ansshul10@gmail.com>
2025-07-05 17:32:43 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -306,7 +306,7 @@ app.use(errorHandler);
 // --- Server Start ---
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
-    logger.info(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+    logger.info(`HURRAY ! Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
 
 // --- Graceful Shutdown ---
